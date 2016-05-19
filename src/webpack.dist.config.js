62596f5f238421a16ff56862571562c9cb085d9e
Alan Souza <alansouzati@gmail.com>
2016-05-19 12:53:29 -0700

    v0.1.23: fixed issue with custom plugins on gulp dist.

--- src/webpack.dist.config.js
+++ src/webpack.dist.config.js
@@ -14,23 +14,26 @@ const env = deepAssign({
   'process.env.NODE_ENV': '"production"'
 }, options.env);
 
-const plugins = [
+const config = options.webpack;
+
+config.plugins = [
   new webpack.DefinePlugin(env),
   new webpack.optimize.OccurenceOrderPlugin()
-  //new webpack.optimize.DedupePlugin()
 ];
 
 if (!argv.skipMinify) {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: {
       warnings: false
     }
   }));
 }
 
-const config = deepAssign({
-  plugins: plugins
-}, options.webpack);
+if (options.webpack.plugins) {
+  options.webpack.plugins.forEach((plugin) =>
+    config.plugins.push(plugin)
+  );
+}
 
 config.resolve.extensions = deepAssign(config.resolve.extensions || [],
   ['', '.js', '.json', '.htm', '.html', '.scss', '.md', '.svg']);
